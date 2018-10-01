bce5f7b2b185dbf9a1d03838e086cdd9a2acc93a
Kai Niebes <kai.niebes@outlook.com>
2018-10-01 15:18:28 +0200

    Added configuration file and configurable cookie agreement checker (e.g. GDPR) The config directory contains a documented configuration.json to configure cookie requirements. These cookie requirements are disabled by default.

--- assets/js/plupload_widget.js
+++ assets/js/plupload_widget.js
@@ -1,6 +1,9 @@
+// Globals
+let isPluploadLoaded = false
+
 // JavaScript equivalent of PHP dirname()
 function dirname(path) {
-    return path.replace(/\\/g,'/').replace(/\/[^\/]*$/, '');;
+  return path.replace(/\\/g, '/').replace(/\/[^\/]*$/, '');;
 }
 
 /**
@@ -15,7 +18,7 @@ function removeFoldersUntilFilesAreTopLevel(files) {
   _basedir = checkIfTopLevel(files)
   while (_basedir != '') {
     files.forEach(file => {
-      file.relativePath = file.relativePath.replace(_basedir,'')
+      file.relativePath = file.relativePath.replace(_basedir, '')
     })
     _basedir = checkIfTopLevel(files)
   }
@@ -32,28 +35,29 @@ function checkIfTopLevel(files) {
   let _basedir = ''
   for (let i = 0; i < files.length; i++) {
     let file = files[i]
-    // If there is a file in top level directory, stop the loop
+      // If there is a file in top level directory, stop the loop
     if (dirname(file.relativePath) == "") break;
 
     // If the loop reaches the last item
     // no file was found in top level
     // so set _basedir to the topmost directory that can be removed
-    if (i == files.length - 1) _basedir = dirname(file.relativePath).substring(0,dirname(file.relativePath).indexOf('/',1))
+    if (i == files.length - 1) _basedir = dirname(file.relativePath).substring(0, dirname(file.relativePath).indexOf('/', 1))
   }
   return _basedir
 }
 
-(function ($) {
-
+(function($) {
+  // Defining the widget
   Drupal.plupload_widget = Drupal.plupload_widget || {};
 
   // Add Plupload events for autoupload and autosubmit.
-  Drupal.plupload_widget.filesAddedCallback = function (up, files) {
-    setTimeout(function () { up.start(); }, 100);
+  Drupal.plupload_widget.filesAddedCallback = function(up, files) {
+    setTimeout(function() {
+      up.start();
+    }, 100);
   };
 
-  Drupal.plupload_widget.uploadCompleteCallback = function (up, files) {
-
+  Drupal.plupload_widget.uploadCompleteCallback = function(up, files) {
     // Save the reduced Plupload Files Object in a new Object to be passed as cookie
     let _newFiles = removeFoldersUntilFilesAreTopLevel(up.files)
 
@@ -68,7 +72,82 @@ function checkIfTopLevel(files) {
     document.cookie = `UploadToken=${JSON.stringify(token)}`;
   };
 
+  // Load configuration file and configure the cookie checking
+  $.getJSON("../../modules/plupload_widget/config/configuration.json", (configuration) => {
+    /**
+     * Checks whether the agreement cookie has the agreed value
+     * If not, force the wrong cookie to expire so the Cookie-Accept-Banner reappears
+     * @return {bool} Cookie Agreed
+     */
+    function checkCookieAgreement() {
+      // Returns all cookies the document has
+      let _cookies = document.cookie.split('; ')
+        // Default value to return
+      let _agreed = false
+        // Find the right cookie and check if the agreement value matches
+      _cookies.forEach(cookie => {
+        if (cookie.indexOf(configuration.cookie.Name) >= 0) {
+          if (cookie.split('=')[1] == configuration.cookie.AgreeVal) {
+            _agreed = true
+          } else {
+            // Force expire the cookie and reload the page, so the user can be prompted again
+            document.cookie = `${configuration.cookie.Name}=undefined;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=${configuration.cookie.Path}`
+            location.reload()
+          }
+        }
+      })
+      return _agreed
+    }
 
+    /**
+     * Load the full plupload widget
+     * @return {[type]} [description]
+     */
+    function initPluploadWidget() {
+      isPluploadLoaded = true
 
+      // Remove cookie reminder message if it exists
+      try {
+        document.getElementById(configuration.cookie.reminderID).outerHTML = ''
+      } catch (e) {
+        console.log('No cookie reminder to be hidden. Cookies were probably already enabled');
+      }
+
+      document.querySelectorAll('.plupload-element').forEach(widget => {
+        widget.style.display = 'block';
+      })
+    }
+
+    // Check if cookie agreement checking is enabled in configuration
+    // If it's enabled, check if the cookie has the agreed value
+    function tryInitPluploadWidget() {
+      if (!configuration.useCookie || (configuration.useCookie && checkCookieAgreement())) {
+        initPluploadWidget()
+      } else {
+        // Configure what happens when cookies are not enabled
+        // alert('Cookies need to be enabled')
+        // In this case all instances of the plupload widget on-site will be replaced with a notification
+        document.querySelectorAll('.plupload-element').forEach(widget => {
+          if (document.getElementById(configuration.cookie.reminderID) == null) {
+            let errorNode = document.createElement('h1')
+            errorNode.innerText = "Cookies need to be enabled"
+            errorNode.id = configuration.cookie.reminderID
+            widget.parentElement.appendChild(errorNode)
+            widget.style.display = 'none';
+          }
+        })
+      }
+    }
+
+    // Initial try
+    tryInitPluploadWidget()
+      // Subsequent tries until the widget can be loaded
+    let checkingInterval = setInterval(() => {
+      if (!isPluploadLoaded) {
+        tryInitPluploadWidget()
+      } else {
+        clearInterval(checkingInterval)
+      }
+    }, configuration.Interval)
+  });
 })(jQuery);
-
